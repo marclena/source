a6ab9f2cc373a832298c4e79976c2033f7d76af0
Marc Cardenas <marc.cardenas@col.vueling.com>
2019-08-05 07:57:30 +0000

    Deleted Vueling.XXX.DB.Infrastructure. No se usara

--- Vueling.XXX.DB.Infrastructure/Repositories/AircraftRepository/IAircraftContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Objects;
-
-namespace Vueling.XXX.DB.Infrastructure.Repositories.AircraftRepository
-{
-    public interface IAircraftContext
-    {
-        ObjectContext Context{ get; }
-    }
-}
