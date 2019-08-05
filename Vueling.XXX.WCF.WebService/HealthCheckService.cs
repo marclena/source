b91977a00614e6f67f1284e294be499ebbaa6359
Marc Cardenas <mcardenas@kiteris.com>
2019-08-05 10:23:40 +0200

    push

---
+++ Vueling.XXX.WCF.WebService/HealthCheckService.cs
@@ -0,0 +1,17 @@
+﻿using ATC.HealthChecks.Wcf;
+using ATC.HealthChecks.Wcf.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vueling.XXX.WCF.WebService
+{
+    public class HealthCheckService : IHealthCheckService
+    {
+        public HealthStatus CheckHealth()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
